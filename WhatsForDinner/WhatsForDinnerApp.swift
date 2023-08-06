cc63e0c805750a5f2de055a06f4a3f6091650f05
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-06 17:23:39 -0400

    Initial Commit

---
+++ WhatsForDinner/WhatsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsForDinnerApp.swift
+//  WhatsForDinner
+//
+//  Created by Chuck Belcher on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
