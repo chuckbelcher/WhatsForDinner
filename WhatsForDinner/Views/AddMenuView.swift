856bca2e9a2499f1606ec683fdf252b8f34fc2a1
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-11 23:01:24 -0400

    Updated add new menu page to include both a text field and a text element for each day.

--- WhatsForDinner/Views/AddMenuView.swift
+++ WhatsForDinner/Views/AddMenuView.swift
@@ -22,30 +22,85 @@ struct AddMenuView: View {
         VStack {
             Text("Add New Menu")
                 .font(.title)
-            
-            TextField("Sunday", text: $sunday)
+                .padding(.bottom)
+            VStack {
+                HStack {
+                    Text("Sunday")
+                        .font(.title3)
+                        .padding(.trailing)
+                    TextField("Add Dinner Items Here ...", text: $sunday)
+                }
                 .padding(.leading, 20)
-                .padding(.bottom, 20)
-            TextField("Monday", text: $monday)
+                .padding(.bottom, 30)
+                
+                HStack {
+                    Text("Monday")
+                        .font(.title3)
+                        .padding(.trailing)
+                    TextField("Add Dinner Items Here ...", text: $monday)
+                }
                 .padding(.leading, 20)
-                .padding(.bottom, 20)
-            TextField("Tuesday", text: $tuesday)
+                .padding(.bottom, 30)
+                
+                HStack {
+                    Text("Tuesday")
+                        .font(.title3)
+                        .padding(.trailing)
+                    TextField("Add Dinner Items Here ...", text: $tuesday)
+                }
                 .padding(.leading, 20)
-                .padding(.bottom, 20)
-            TextField("Wednesday", text: $wednesday)
+                .padding(.bottom, 30)
+                
+                HStack {
+                    Text("Wednesday")
+                        .font(.title3)
+                        .padding(.trailing)
+                    TextField("Add Dinner Items Here ...", text: $wednesday)
+                }
                 .padding(.leading, 20)
-                .padding(.bottom, 20)
-            TextField("Thursday", text: $thursday)
+                .padding(.bottom, 30)
+                
+                HStack {
+                    Text("Thursday")
+                        .font(.title3)
+                        .padding(.trailing)
+                    TextField("Add Dinner Items Here ...", text: $thursday)
+                }
                 .padding(.leading, 20)
-                .padding(.bottom, 20)
-            TextField("Friday", text: $friday)
+                .padding(.bottom, 30)
+                
+                HStack {
+                    Text("Friday")
+                        .font(.title3)
+                        .padding(.trailing)
+                    TextField("Add Dinner Items Here ...", text: $friday)
+                }
                 .padding(.leading, 20)
-                .padding(.bottom, 20)
-            TextField("Saturday", text: $saturday)
+                .padding(.bottom, 30)
+                
+                HStack {
+                    Text("Saturday")
+                        .font(.title3)
+                        .padding(.trailing)
+                    TextField("Add Dinner Items Here ...", text: $saturday)
+                }
                 .padding(.leading, 20)
-                .padding(.bottom, 20)
-        
-            Spacer()
+                .padding(.bottom, 30)
+                
+                Spacer()
+                
+                Button {
+                    print("Submitting New Menu")
+                } label: {
+                    Text("Submit New Menu")
+                        .padding(.horizontal, 50)
+                        .padding(.vertical, 25)
+                        .font(.title3)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(50)
+                }
+            }
 
         }
         .padding()
